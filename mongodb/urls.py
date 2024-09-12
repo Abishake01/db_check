ff5388393cdd3530c42dfab7626a899f42712c71
Abishake01 <abishake381@gmail.com>
2024-09-12 19:38:46 +0530

    checking database

--- mongodb/urls.py
+++ mongodb/urls.py
@@ -4,6 +4,5 @@ from  . import views
 
 urlpatterns=[
     path('',views.index),
-    path('add',views.add_person),
-    path('show',views.get_all_person)
+   
 ]
--- mongodb/views.py
+++ mongodb/views.py
@@ -8,14 +8,4 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h1>App is running...</h1>')
 
-def add_person(request):
-    records={
-        'f_n':'abi',
-        'l_n':'mike'
-    }
-    person_coll.insert_one(records)
-    return HttpResponse('New Person is added')
-
-def get_all_person(request):
-    persons=person_coll.find()
-    return HttpResponse(persons)
+ 
